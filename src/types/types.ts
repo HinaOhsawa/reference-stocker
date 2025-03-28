d0bd43cd3b86148bf5a1d5baa875806c12985a18
hina <hinadegozaru888@gmail.com>
2025-03-28 16:14:04 +0900

    postのinterfaceを作成

---
+++ src/types/types.ts
@@ -0,0 +1,11 @@
+export interface Post {
+  id: string;
+  title: string;
+  url: string;
+  memo: string;
+  userId: string;
+  published: boolean;
+  user: string;
+  createdAt: string;
+  updatedAt: string;
+}
