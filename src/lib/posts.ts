a3a792aa1d526a000dd4a36e3b4ab959ff32fc2d
hina <hinadegozaru888@gmail.com>
2025-03-29 12:24:23 +0900

    postの詳細を取得するapi呼び出し関数を作成

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -6,3 +6,12 @@ export async function getAllPosts() {
   if (!res.ok) throw new Error("Failed to fetch posts");
   return res.json();
 }
+
+// Idを使ってpostの詳細を取得
+export async function getPostDetail(id: string) {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts/${id}`, {
+    cache: "no-store",
+  });
+  const post: Post = await res.json();
+  return post;
+}
