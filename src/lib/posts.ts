d9f89948887b29d2569832ab42184bd95eed3969
hina <hinadegozaru888@gmail.com>
2025-03-29 12:22:12 +0900

    すべてのpostを取得する関数名をgetAllPostsに変更

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,4 +1,7 @@
-export async function getPosts() {
+import { Post } from "@/types/types";
+
+// すべてのpostを取得
+export async function getAllPosts() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts`);
   if (!res.ok) throw new Error("Failed to fetch posts");
   return res.json();
