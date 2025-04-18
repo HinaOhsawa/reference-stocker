388d7ec40e994efb74759f52deb690764dad336a
hina <hinadegozaru888@gmail.com>
2025-04-18 11:38:31 +0900

    PostCardコンポーネントにリンクカードを追加、リンクの競合のためクライアント処理に変更

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+"use client";
 import {
   Card,
   CardContent,
@@ -12,58 +12,74 @@ import { Post } from "@/types/types";
 import dayjs from "dayjs";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { BookmarkButton } from "./BookmarkButton";
-import { auth } from "@/lib/auth";
-import { prisma } from "@/lib/prismaClient";
 import { User } from "lucide-react";
+import LinkCard from "./LinkCard";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { useEffect, useState } from "react";
 
 interface DataProps {
   PostData: Post;
 }
 
-const PostCard = async ({ PostData }: DataProps) => {
+const PostCard = ({ PostData }: DataProps) => {
   const { id, title, url, createdAt, user, tags } = PostData; // 分割代入
 
-  // 現在のユーザーがこの投稿をブックマークしているかチェック
-  const session = await auth();
-  const userId = session?.user?.id;
-  const existingBookmark = userId
-    ? !!(await prisma.bookmark.findFirst({ where: { userId, postId: id } }))
-    : false;
+  const router = useRouter();
+
+  const [bookmarked, setBookmarked] = useState(false);
+
+  useEffect(() => {
+    const fetchBookmark = async () => {
+      const res = await fetch(`/api/posts/bookmark/${id}`);
+      const data = await res.json();
+      setBookmarked(data.bookmarked);
+      console.log(data);
+    };
+    fetchBookmark();
+  }, [id]);
+
+  //カードごとリンクにしたい場合クライアントコンポーネントにする
+  const handleClick = () => {
+    router.push(`/post/${id}`);
+  };
 
   return (
-    <Link href={`/post/${id}`} className="text-blue-500">
-      <Card>
-        <CardHeader>
+    <Card onClick={handleClick}>
+      <CardHeader>
+        <Link href={`/post/${id}`}>
           <CardTitle>{title}</CardTitle>
-          <small>{dayjs(createdAt).format("YYYY/MM/DD HH:mm")}</small>
+        </Link>
+        <small>{dayjs(createdAt).format("YYYY/MM/DD HH:mm")}</small>
 
-          <CardDescription>
-            {" "}
-            <div className="flex items-center gap-4">
-              <Avatar className="w-8 h-8">
-                <AvatarImage src={user.image ?? undefined} alt="@username" />
-                <AvatarFallback className="bg-gray-100 text-gray-400">
-                  <User size={40} />
-                </AvatarFallback>
-              </Avatar>
-              <p>{user.name}</p>
-            </div>
-          </CardDescription>
-        </CardHeader>
-        <CardContent>{url}</CardContent>
-
-        <CardFooter className="flex flex-wrap gap-2 mb-2">
-          <div className="flex justify-end items-center gap-2">
-            {tags?.map((tag) => (
-              <span key={tag.id} className="tag">
-                {tag.name}
-              </span>
-            ))}
-            <BookmarkButton postId={id} initialBookmarked={existingBookmark} />
+        <CardDescription>
+          {" "}
+          <div className="flex items-center gap-4">
+            <Avatar className="w-8 h-8">
+              <AvatarImage src={user.image ?? undefined} alt="@username" />
+              <AvatarFallback className="bg-gray-100 text-gray-400">
+                <User size={40} />
+              </AvatarFallback>
+            </Avatar>
+            <p>{user.name}</p>
           </div>
-        </CardFooter>
-      </Card>
-    </Link>
+        </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <LinkCard url={url} />
+      </CardContent>
+
+      <CardFooter className="flex flex-wrap gap-2 mb-2">
+        <div className="flex justify-end items-center gap-2">
+          {tags?.map((tag) => (
+            <span key={tag.id} className="tag">
+              {tag.name}
+            </span>
+          ))}
+          <BookmarkButton postId={id} initialBookmarked={bookmarked} />
+        </div>
+      </CardFooter>
+    </Card>
   );
 };
 
