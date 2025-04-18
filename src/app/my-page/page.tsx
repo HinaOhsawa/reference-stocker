ca4d74a6b8b1782deccf92ed980dabbf8dbe3b80
hina <hinadegozaru888@gmail.com>
2025-04-18 11:43:07 +0900

    マイページにブックマークリンクを追加

--- src/app/my-page/page.tsx
+++ src/app/my-page/page.tsx
@@ -1,6 +1,7 @@
 import MyPostCard from "@/components/MyPostCard";
 import { auth } from "@/lib/auth";
 import { getMyPosts } from "@/lib/user";
+import Link from "next/link";
 
 export default async function MyPage() {
   const session = await auth();
@@ -16,6 +17,7 @@ export default async function MyPage() {
     <div>
       <h2 className="text-xl font-bold">マイページ</h2>
       <h3>投稿一覧</h3>
+      <Link href="/my-page/bookmark">ブックマーク</Link>
       <ul className="grid px-4 py-4 gap-4">
         {myPosts.map((Post) => (
           <MyPostCard key={Post.id} PostData={Post} />
