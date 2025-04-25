891219d49355e1c9a415f99fff5b5c262a124538
hina <hinadegozaru888@gmail.com>
2025-04-25 15:06:27 +0900

    ブックマークボタンのログインの確認と、スタイル変更

--- src/components/BookmarkButton.tsx
+++ src/components/BookmarkButton.tsx
@@ -4,10 +4,14 @@ import { useEffect, useState, useTransition } from "react";
 import { toggleBookmark } from "@/app/actions/bookmarkAction";
 import { BookmarkCheck, BookmarkIcon } from "lucide-react";
 import { Button } from "./ui/button";
+import { useSession } from "next-auth/react";
+import { toast } from "sonner";
+import { LoadingSpinner } from "./LoadingSpinner";
 
 type Props = { postId: string; initialBookmarked: boolean };
 
 export function BookmarkButton({ postId, initialBookmarked }: Props) {
+  const { data: session } = useSession();
   // 親から受け取った initialBookmarked を反映する
   const [bookmarked, setBookmarked] = useState(Boolean(initialBookmarked));
   useEffect(() => {
@@ -19,6 +23,11 @@ export function BookmarkButton({ postId, initialBookmarked }: Props) {
   const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.stopPropagation(); // クリックの伝播を止める
     e.preventDefault(); // リンク遷移も防止（必要なら）
+
+    if (!session) {
+      toast.warning("ログインが必要です");
+      return;
+    }
     startTransition(async () => {
       const newState = await toggleBookmark(postId);
       setBookmarked(newState);
@@ -30,16 +39,16 @@ export function BookmarkButton({ postId, initialBookmarked }: Props) {
       variant="secondary"
       onClick={handleClick}
       disabled={isPending}
-      className={`text-xl mb-4 rounded-full cursor-pointer hover:bg-gray-200 transition ${
+      className={`w-10 h-10 text-xl mb-4 rounded-full cursor-pointer border bg-white hover:bg-gray-200 transition ${
         bookmarked ? "bg-blue-500 text-white" : ""
       }`}
     >
       {isPending ? (
-        "処理中..."
+        <LoadingSpinner />
       ) : bookmarked ? (
         <BookmarkCheck />
       ) : (
-        <BookmarkIcon className="w-10 h-10" />
+        <BookmarkIcon className="" />
       )}
     </Button>
   );
