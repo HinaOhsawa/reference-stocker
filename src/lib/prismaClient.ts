c9aa5ea6701114c46af35be156aa020d6f442feb
hina <hinadegozaru888@gmail.com>
2025-04-29 16:22:52 +0900

    本番環境で接続が安定するように変更

--- src/lib/prismaClient.ts
+++ src/lib/prismaClient.ts
@@ -1,7 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+let prisma: PrismaClient;
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+if (process.env.NODE_ENV === "production") {
+  // 本番環境では新しいPrismaClientを作成
+  prisma = new PrismaClient();
+} else {
+  // 開発（localhost）ではglobalにキャッシュする
+  const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient();
+  }
+  prisma = globalForPrisma.prisma;
+}
+
+export { prisma };
