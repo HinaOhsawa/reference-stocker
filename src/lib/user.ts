556362c45c4b66064d70107e1055b2ab8c67569c
hina <hinadegozaru888@gmail.com>
2025-05-13 11:50:40 +0900

    不要なAPIを削除

--- src/lib/user.ts
+++ src/lib/user.ts
@@ -1,4 +1,3 @@
-import { Bookmark, Post } from "@/types/types";
 import { prisma } from "@/lib/prismaClient";
 import { auth } from "@/lib/auth";
 
@@ -16,26 +15,6 @@ export async function fetchUser() {
   }
   const user = await res.json();
   return user;
-}
-
-// 自分の投稿を取得
-export async function fetchMyPosts() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/my-posts`, {
-    cache: "no-store",
-  });
-  if (!res.ok) throw new Error("Failed to fetch posts");
-  const myPosts = await res.json();
-  const posts: Post[] = myPosts.map((post: Post) => post);
-  return posts;
-}
-
-// bookmark記事を取得
-export async function fetchMyBookmark() {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/my-bookmark`);
-  if (!res.ok) throw new Error("Failed to fetch posts");
-  const bookmarks = await res.json();
-  const posts: Post[] = bookmarks.map((bookmark: Bookmark) => bookmark.post);
-  return posts;
 }
 
 // プロフィール画像の更新
